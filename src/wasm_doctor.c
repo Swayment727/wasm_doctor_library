59718cba697e98c7dbfabfa0362a648ee3c8afcc
Jakub Mares <kubam.mares@gmail.com>
2024-04-28 18:43:31 +0200

    Add documentation

--- src/wasm_doctor.c
+++ src/wasm_doctor.c
@@ -64,30 +64,47 @@ doctor_load(wasmptr_t address, uint32_t bit_size)
         check_use_after_free(&doctor->heap_validator, address * 8, bit_size);
 }
 
+/**
+ * @param[in] block_start Start address of the allocated block.
+ * @param[in] size_in_bytes Size of the allocated block.
+ */
 void
 doctor_register_malloc(wasmptr_t block_start, uint32_t size_in_bytes)
 {
         register_malloc(&doctor->heap_validator, block_start, size_in_bytes);
 }
 
+/**
+ * @param[in] block_start Start address of the allocated block.
+ */
 void
 doctor_register_free(wasmptr_t block_start)
 {
         register_free(&doctor->heap_validator, block_start);
 }
 
+/**
+ * @param[in] idx Index of the local which is being set by the local.set WebAssembly instruction.
+ */
 void
 doctor_local_set(uint32_t idx)
 {
         register_set(&doctor->local_validator, idx);
 }
 
+/**
+ * @param[in] idx Index of the local which is accessed by the local.get WebAssembly instruction.
+ */
 void
 doctor_local_get(uint32_t idx)
 {
         validate_get(&doctor->local_validator, idx);
 }
 
+/**
+ * @param[in] locals_size The number of locals a WebAssembly function (frame) has.
+ * @param[in] function_name The name of function that is being entered.
+ */
 void
 doctor_frame_enter(uint32_t locals_size, char *function_name)
 {
@@ -103,6 +120,7 @@ doctor_frame_exit(void)
 }
 
 /**
+ * @param[in, out] wasm_doctor Wasm Doctor struct containing all validators and reports.
  * @param[in] size_in_pages Size of WebAssembly memory in pages.
  */
 void
@@ -116,12 +134,19 @@ doctor_init(struct wasm_doctor *wasm_doctor, uint32_t size_in_pages)
         mem_addr_validator_init(&doctor->mem_validator, WASM_PAGE_SIZE * size_in_pages * 8, &doctor->reporter);
 }
 
+/**
+ * Print error report.
+ */
 void
 doctor_report(void)
 {
         report(&doctor->reporter);
 }
 
+/**
+ * Exit reporter if it was not previously done by doctor_exit. This may happens in case the reports are needed after the
+ * analysis has already stopped.
+ */
 void
 doctor_reporter_exit(void)
 {
@@ -129,6 +154,10 @@ doctor_reporter_exit(void)
         wasm_state_exit(&doctor->state);
 }
 
+/**
+ * @param[in] exit_reporter Boolean value decidig whether to call reporter_exit or keep the reports for further use
+ * (i.e. testing).
+ */
 void
 doctor_exit(bool exit_reporter)
 {
